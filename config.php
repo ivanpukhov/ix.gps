b8f4feddbf7804cedd84b22f5cef2358f3d34e9d
Ivan Pukhov <ix.ivanp@gmail.com>
2022-09-19 19:35:02 +0300

    отслеживание пользователя на js + php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_USER', 'ivaninbox');
+
+define('DB_PASSWORD', 'ivaninbox_wp_1');
+
+define('DB_HOST', 'localhost');
+
+define('DB_NAME', 'ivaninbox_wp_1');
+
+define('COMP_NAME', 'gps');
+
+define('GMAPS_APIKEY', 'AIzaSyDGjRFvMzZpyW1Dva4_6WWqcVF1NyMS2qE');
+
+?>
